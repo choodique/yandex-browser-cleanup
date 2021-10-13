5e5006df637aefafc8d476c9ce594608efe334ea
choodique <choodique@pm.me>
2021-10-14 00:43:45 +0300

    First commit

---
+++ yandex-browser-cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf /Applications/Yandex.app
+rm -rf ~/Library/Application\ Support/Yandex/YandexBrowser
+rm -rf ~/Library/Caches/Yandex/YandexBrowser
+rm -rf ~/Library/Preferences/ru.yandex.desktop.yandex-browser.plist
+rm -rf ~/Library/Saved\ Application\ State/ru.yandex.desktop.yandex-browser.savedState
+rm -rf ~/.yandex
